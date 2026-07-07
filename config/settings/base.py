e025115eb2e7eaef9f541fd81bf6163692fbf4ce
David Gaitan <jdavid.gaitan@gmail.com>
2026-07-06 21:41:58 -0600

    Fix STATICFILES_DIRS so cc/static/ is collected in production

--- config/settings/base.py
+++ config/settings/base.py
@@ -117,6 +117,7 @@ MIDDLEWARE = [
 # ------------------------------------------------------------------------------
 STATIC_ROOT = str(BASE_DIR / "staticfiles")
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [str(APPS_DIR / "static")]
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
